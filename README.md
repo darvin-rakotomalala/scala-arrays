b6c4d20396e5ead83f20396d2f1d80a0eac59897
darvin <darvintojo@gmail.com>
2025-01-17 09:10:48 -0500

    Update file

---
+++ README.md
@@ -0,0 +1,8 @@
+## Scala | Arrays
+
+
+### Technologies
+
+- Java 17
+- Sbt 1.10.17
+- Scala 3.3.4
---
+++ src/main/scala/main.scala
@@ -0,0 +1,5 @@
+@main
+def main(): Unit =
+  println("------------------------------------------------")
+  println("Hello world! This is an example of the Scala Arrays")
+
